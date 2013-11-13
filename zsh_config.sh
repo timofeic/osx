7d627f3ae5f20587aded68c5a6ba6b933df693bc
Tim Cheung <thcheung@gmail.com>
2013-11-13 11:26:40 +0000

    Initial commit for oh-my-zsh install

---
+++ zsh_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# depends on brew.sh - to install zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.zshrc ~/.zshrc.orig
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+
