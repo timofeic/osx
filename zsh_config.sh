27d009334a3c481af19916788fbf3893f823e591
Tim Cheung <timofei.c@gmail.com>
2020-10-01 13:26:27 -0700

    Update zsh_config.sh
    add theme

--- zsh_config.sh
+++ zsh_config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # depends on brew.sh - to install zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 cp ~/.zshrc ~/.zshrc.orig
 cp zshrc_tim ~/.zshrc
 chsh -s /bin/zsh
