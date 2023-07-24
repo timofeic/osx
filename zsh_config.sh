f8276d854f0dc8d3fb641ce995481bd3e3992c9c
Tim Cheung <tim@f7i.ai>
2023-07-24 15:32:50 +1000

    update autosuggestions repo

--- zsh_config.sh
+++ zsh_config.sh
@@ -2,8 +2,8 @@
 # depends on brew.sh - to install zsh
 /bin/bash -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 cp ~/.zshrc ~/.zshrc.orig
 cp zshrc_tim ~/.zshrc
 chsh -s /bin/zsh
